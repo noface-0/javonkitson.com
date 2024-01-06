0ee9f6d116a16a1582c23652c4be1c75c586e842
noface <kidc12@gmail.com>
2024-01-06 02:07:03 +0000

    file overwrite correctio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+// import logo from './logo.svg';
 import React from "react";
-
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/index";
-
 import "./App.css";
-
+import { Routes, Route, } from "react-router-dom";
+import Home from "./pages/index";
+import ThemeContext from "./components/theme";
 
 function App() {
   return (
+    <ThemeContext>
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+    </ThemeContext>
   );
 }
 
